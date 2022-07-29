069052923c0d0b01aee1e0d96e140e1bd3b5d418
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-07-29 11:37:47 -0300

    Criando estilos da rota

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import '../assets/css/reset.css';
+import MainRoute from './MainRoute/MainRoute';
+
+export default function App(){
+    return (
+        <>
+        <MainRoute />
+        </>
+    );
+}
---
+++ src/components/MainRoute/MainRoute.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function MainRoute(){
+    return (
+        <>
+            <div className='navBar'>CINEFLEX</div>
+            <div className='text'>Selecione o filme</div>
+        </>
+    );
+}
