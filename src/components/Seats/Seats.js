a047387dd773c96b972eb13c8343541f4f06f250
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-07-30 15:20:28 -0300

    Selecionando Assentos

--- src/components/Seats/Seats.js
+++ src/components/Seats/Seats.js
@@ -1,8 +1,19 @@
 import './style.css';
+import { useState } from 'react';
 
 export default function Seats({data}){
 
+    const [classSeat, setClassSeat] = useState('seat available');
+
+    function availableSeats(seat){
+        if(seat === false) alert('Esse assento não está disponível');
+        else{
+            if(classSeat === 'seat selected') setClassSeat('seat available');
+            else setClassSeat('seat selected');
+        }
+    }
+
     return (
-        <div className={data.isAvailable ? 'seat available' : 'seat unavailable'}>{data.name}</div>
+        <div onClick={() => availableSeats(data.isAvailable)} className={data.isAvailable ? classSeat : 'seat unavailable'}>{data.name}</div>
     );
 }
